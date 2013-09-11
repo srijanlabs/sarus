32916c93cd292b50829dd92e74e9f64b452684f9
fotuzlab <arijitdutta@fotuzlab.in>
2013-09-11 18:09:46 +0530

    anchor, changes data on page on scroll own, json integrated through node

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -17,14 +17,20 @@ angular.module('myApp.controllers', []).
     });
 
   }).
-  controller('MyCtrl1', function ($scope, $location) {
-    // Scroll to set alert
-    window.onscroll = function(ev) {
-      if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        $location.path('/view2');
-        $scope.$apply();
-      }
-    };
+  controller('BlogCtrl', function ($scope, $rootScope, $location, $http) {
+    // Get data from JSON.
+    $http.get('/api/blogs').success(function(data) {
+      $scope.blogs = data[0];
+      console.log(data[0]);
+    
+      // Scroll to set alert
+      window.onscroll = function(ev) {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+          $scope.blogs = data[1]
+          $scope.$apply();
+        }
+      };
+    });
   }).
   controller('MyCtrl2', function ($scope) {
     // write Ctrl here
