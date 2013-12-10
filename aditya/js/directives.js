0fa71316cc773ff8a79a5b11fe76f760cc93fd4b
Aditya Raj <aditya.r@srijan.in>
2013-12-10 16:08:43 +0530

    Fixed a few issues and removed debugger.

--- aditya/js/directives.js
+++ aditya/js/directives.js
@@ -18,7 +18,6 @@ angular.module('two1App.directives', []).
                     window.onscroll = function(ev) {
             if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
                 scope.$apply(attr.scrollLoad);
-                debugger;
             }
           }
         //});
