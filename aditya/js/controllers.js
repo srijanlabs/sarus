b444079719fa2255acbdf634c1cfc41c22b7de9e
Aditya Raj <aditya.r@srijan.in>
2013-12-20 19:09:45 +0530

    Fixed issue with scroll and inview. Also implemented htauth with rss feed.

--- aditya/js/controllers.js
+++ aditya/js/controllers.js
@@ -36,23 +36,35 @@ angular.module('two1App.controllers', []).controller('PostsController', function
 
     $scope.loadPosts();
 
-    $scope.changeUrl = function(slug){
-        $location.path("/"+slug)
-    }
+    var slugs = [];
+    $scope.changeUrl = function(slug, index, inview, inviewpart){
+      slugs[index] = slug;
+      if(inview == true){
+        $location.path("/"+slug);
+      }
+      console.log("s: "+ slug +" v:"+inview+ " p:"+inviewpart);
+      var pslug = slugs[index-1];
+      if(inview == false && angular.isUndefined(pslug) == false){ // Assuming that inview false means that the current slug is going out of the view by scrolling up
+        $location.path("/"+pslug);
+      }
+    };
 
     var cnt = 0;
-    $scope.loadNext = function(){
+    $scope.loadNext = function(inview){
+      if(inview == false){return false;}
       cnt +=1;
       $http.defaults.useXDomain = true;
       var httpRequest = $http({
           method: 'GET',
           url: 'http://qz.local:3000/'+cnt,
       }).success(function(data, status) {
+              if(data == ""){return false;}
               $scope.posts.push(data);
       });
     };
 
     $scope.getSlug = function( str ) {
+      if(angular.isUndefined(str)){return "";}
       return str.split( "/" ).filter(function( n ) {
         return n; // What is this supposed to do?
       }).reverse()[ 0 ];
