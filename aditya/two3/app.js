2b502aa51cbab2cc5c6d4dfd23659996741683d2
Aditya Raj <aditya.r@srijan.in>
2013-12-23 19:43:17 +0530

    Implemented node.js service to return post after a specific slug.

--- aditya/two3/app.js
+++ aditya/two3/app.js
@@ -3,6 +3,8 @@ var feed_url = "http://staging.srijan7v2.srijan-sites.com/blog-feeds.xml"//"http
 var options = {'auth': {'user': "j2r@srijan.in", 'pass': "123", 'sendImmediately': false}};
 var http = require('http');
 var express = require('express');
+var RequestCaching = require('node-request-caching');
+var rc = new RequestCaching();
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -10,6 +12,31 @@ var allowCrossDomain = function(req, res, next) {
 
     next();
 }
+
+var getSlug = function( str ) {
+      return str.split( "/" ).filter(function( n ) {
+        return n;
+      }).reverse()[ 0 ];
+    };
+
+
+var takeStartingAt = function (data, start) {
+    console.log(data);
+    console.log(start);
+  var result = [];
+  var skip = true;
+  for (var i = 0; i < data.length; i++) {
+    if (getSlug(data[i].url) === start) {
+      skip = false;
+    }
+    if (skip) {
+      continue;
+    }
+    result.push(data[i]);
+  }
+  return result;
+
+};
 
 var app = express();
 
@@ -26,6 +53,23 @@ app.get('/:count', function(req, res, next){
       res.end(JSON.stringify(article, null, 2));
     });
 });
+
+app.get('/offset/:slug/:count', function(req, res, next){
+    //var request = require('request');
+    //var resp = request.get(feed_url).auth('j2r@srijan.in', '123', false);
+    parser.parseURL(feed_url, options, function(err, out){
+      if(out == null){console.log("Request failed !!");return false;}
+      var count = req.params.count;
+      var slug = req.params.slug;
+      //console.log("count:"+count+" slug:"+slug);
+      var offsetItems = takeStartingAt(out.items, slug);
+      console.log();
+      var article = count != "all" ? offsetItems[count] : offsetItems;
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      res.end(JSON.stringify(article, null, 2));
+    });
+});
+
 app.listen(3000, '127.0.0.1');
 
 
---
+++ node_modules/node-rss/lib/node-rss.js
@@ -0,0 +1,84 @@
+﻿
+/**
+  * this is an RSS module for node. 
+  **/
+
+var libxml = require('libxmljs');
+
+
+  // this function will return a "Feed" object, which can then have items added to it
+exports.createNewFeed = function (title, link, desc, author, feedLink, options) {
+    var feed = {};
+    feed.title = title;
+    feed.link = link;
+    feed.description = desc;
+    feed.defaults = { author: author, cdata : ['description', 'title'] };
+    feed.items = [];
+    feed.language = 'en-US';
+    feed.feedLink = feedLink;
+    for (var opt in options) {
+        feed[opt] = options[opt];
+    }
+
+    feed.addNewItem = function (itemTitle, itemLink, pubDate, description, fields) {
+        var item = {
+            title: itemTitle,
+            link: itemLink,
+            pubDate: pubDate,
+            description: description,
+            guid: itemLink,
+            author: this.defaults.author
+        };
+        for (var field in fields) {
+            item[field] = fields[field];
+        }
+
+        this.items.push(item);
+    };
+
+    return feed;
+};
+
+exports.getFeedXML = function (feed) {
+    var doc = new libxml.Document("1.0", "utf-8");
+    var root = doc.node('rss');
+    var rssNodeProps = {
+        'xmlns:content': 'http://purl.org/rss/1.0/modules/content/',
+        'xmlns:wfw': 'http://wellformedweb.org/CommentAPI/',
+        'xmlns:dc': 'http://purl.org/dc/elements/1.1/',
+        'xmlns:atom': 'http://www.w3.org/2005/Atom',
+        'xmlns:sy': 'http://purl.org/rss/1.0/modules/syndication/',
+        'xmlns:slash': 'http://purl.org/rss/1.0/modules/slash/'
+    };
+    root.attr({ 'version': '2.0' });
+    for (var i in rssNodeProps) {
+        root.attr(i, rssNodeProps[i]);
+    }
+    var channel = root.node('channel');
+    for (var nd in feed) {
+        if (nd != 'items' && nd != 'defaults' && nd != 'feedLink' && nd != 'addNewItem') {
+            if (feed.defaults.cdata.indexOf(nd) == -1) {
+                channel.node(nd, feed[nd]);
+            } else {
+                channel.node(nd).cdata(feed[nd]);
+            }
+        }
+    }
+    var aLink = channel.node('atom:link');
+    aLink.attr({ href: feed.feedLink, rel: 'self', type: 'application/rss+xml' });
+    var now = new Date();
+    channel.node('lastBuildDate', now.toString());
+    
+    feed.items.forEach(function (item) {
+        var itemNode = channel.node('item');
+        
+        for (var nd in item) {
+            if (feed.defaults.cdata.indexOf(nd) == -1) {
+                itemNode.node(nd, item[nd]);
+            } else {
+                itemNode.node(nd).cdata(item[nd]);
+            }
+        }
+    });
+    return doc.toString();
+};
