aee219319d14a9307ff9dbde9503adb82cd17b0f
Aditya Raj <aditya.r@srijan.in>
2014-01-08 00:50:26 +0530

    Fixed and added back html5 routing

--- aditya/js/app.js
+++ aditya/js/app.js
@@ -8,4 +8,16 @@ var app = angular.module('two1App', [
   'ngSanitize',
   'ngLoadScript'
 
-]);
+]).// Route configuration below
+config(function ($routeProvider, $locationProvider, $disqusProvider) {
+  $locationProvider.html5Mode(true);//Required to avoid urls with # anchor tags
+
+  $routeProvider.
+    when('/:slug', {
+      //templateUrl: 'partials/post',
+      controller: 'PostController'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+});
