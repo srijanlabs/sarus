e0d230f3b3e1d33fd65889929fc3ec0073f10d19
Ajay Singh <meajaysingh@hotmail.com>
2014-08-02 21:09:21 +0530

    setting up directories for angular app

---
+++ app/scripts/directives/directives.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/* Directives */
+
+angular.module('sarusApp.directives', [])
+    .directive('repeatDone', function() {
+        return function(scope, element, attrs) {
+            if (scope.$last) {
+                // all are rendered
+                scope.$eval(attrs.repeatDone);
+            }
+        }
+    })
+    .directive('whenScrolled', function() {
+        // Runs during compile
+        return {
+            scope: {
+                whenScrolled: "&",
+                scrollOffSet: "@"
+            },
+            restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+            link: function($scope, iElm, iAttrs, controller) {
+                var raw = iElm[0];
+                var offset= parseInt($scope.scrollOffSet);
+                iElm.bind('scroll', function() {
+                    if (raw.scrollTop + raw.offsetHeight+300 >= raw.scrollHeight  ) {
+                        $scope.$apply($scope.whenScrolled);
+
+                    }
+
+                });
+                console.log(raw.scrollHeight + "   " + raw.scrollTop);
+            }
+        };
+    });
