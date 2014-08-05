56db605ea4158f087658574052e8b20ccfe15a42
Ajay Singh <meajaysingh@hotmail.com>
2014-08-05 16:38:14 +0530

    removing busy sync system , callback  in load more feed ,no need of available, pushing data rather than assign to a index in array full_article

--- app/scripts/services/feedFactory.js
+++ app/scripts/services/feedFactory.js
@@ -5,21 +5,15 @@ angular.module('sarusApp.factories', [])
     .factory('Feed', function($http) {
         // Constructor Function
         function Feed() {
-            //to store ALL feed objects
             this.articles = [];
             this.full_articles = [];
-            this.busy = false;
-            //to sync between server api calls
             this.url = "http://localhost:3000/api/";
-
         }
 
         Feed.prototype = {
 
             initial_loading: function(offset, count, arr_Articles) {
                 var scope = this;
-                if (scope.busy) return;
-                scope.busy = true;
                 $http.defaults.useXDomain = true;
                 $http({
                     method: 'GET',
@@ -27,27 +21,17 @@ angular.module('sarusApp.factories', [])
                 }).success(function(data, status) {
                     for (var i = 0; i < data.length; i++) {
                         var current = data[i];
-                        current.available = false;
                         scope.articles.push(current);
                     }
-                    scope.busy = false;
-
                     arr_Articles.forEach(function(el) {
                         scope.render_Article(scope.articles[el].index);
                     });
                 });
-
-
-                // TODO :: Adding on error also;
-                scope.busy = false;
             },
 
             //Above function wil be called while scrolling down by side bar after injecting it into controller.
-            load_more_feed: function() {
+            load_more_feed: function(callback) {
                 var scope = this;
-                if (scope.busy) return;
-                scope.busy = true;
-                //$http make request to get next 5 feeds[only title, url, index] and push than in ARTICLES;#
                 var offset = scope.articles[scope.articles.length - 1].index + 1;
                 $http.defaults.useXDomain = true;
                 $http({
@@ -56,34 +40,23 @@ angular.module('sarusApp.factories', [])
                 }).success(function(data, status) {
                     for (var i = 0; i < data.length; i++) {
                         var current = data[i];
-                        current.available = false;
                         scope.articles.push(current);
-                    }
-                    scope.busy = false;
-                });
 
-                // TODO :: Adding on error also;
-                scope.busy = false;
+                    }
+                    callback(true);
+                });
 
             },
 
             load_Article: function(index) {
                 var scope = this;
-                if (scope.busy) return;
-                scope.busy = true;
-                //request for that article and update the extra data in ARTICLES[index]#
                 $http.defaults.useXDomain = true;
                 $http({
                     method: 'GET',
                     url: scope.url + "article/" + index
                 }).success(function(data, status) {
-                    scope.articles[index].available = true;
-                    scope.full_articles[index] = data;
-                    scope.busy = false;
+                    scope.full_articles.push(data);
                 });
-
-                // TODO ::: Adding on error also;
-                scope.busy = false;
 
             },
 
@@ -93,8 +66,12 @@ angular.module('sarusApp.factories', [])
             render_Article: function(index) {
                 var scope = this;
                 // TODO ::: Modify logic here
-                if (scope.articles[index].available) return true;
-                scope.load_Article(index);
+                if (!scope.articles[index]) scope.load_more_feed(function(done) {
+                    scope.load_Article(index);
+                });
+                else
+                    scope.load_Article(index);
+
                 return true;
             },
 
