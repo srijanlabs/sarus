6b6f8a69b769e0d8f88215c06f6a74cc3e2554ca
Ajay Singh <meajaysingh@hotmail.com>
2014-08-06 15:47:37 +0530

    directly calling controller function rather than calling and digesting everythong

--- app/scripts/directives/directives.js
+++ app/scripts/directives/directives.js
@@ -24,7 +24,7 @@ angular.module('sarusApp.directives', [])
                 var offset = parseInt($scope.scrollOffSet);
                 iElm.bind('scroll', function() {
                     if (raw.scrollTop + raw.offsetHeight + offset >= raw.scrollHeight) {
-                        $scope.$apply($scope.whenScrolled);
+                        $scope.$eval($scope.whenScrolled);
 
                     }
 
