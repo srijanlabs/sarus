508203aabb8e25cc60946330c0a3f85151cb0aea
Ajay Singh <meajaysingh@hotmail.com>
2014-08-14 13:17:28 +0530

    modularize config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,35 +1,37 @@
-/**
- * Angular app file.
- */
 (function() {
+    /**
+     * Angular app file.
+     */
+
+    angular.module('sarusApp', [
+        // Define dependencies.
+        'ngRoute',
+        'angular-inview',
+        'sarusApp.controllers',
+        'sarusApp.directives',
+        'ngSanitize',
+        'ngLoadScript',
+        'sarusApp.factories'
 
 
-        angular.module('sarusApp', [
-            // Define dependencies.
-            'ngRoute',
-            'angular-inview',
-            'sarusApp.controllers',
-            'sarusApp.directives',
-            'ngSanitize',
-            'ngLoadScript',
-            'sarusApp.factories'
+    ]).config(config);
+    // Route configuration below.
+    function config($routeProvider, $locationProvider) {
+        // HTML5 mode Required to avoid urls with # anchor tags.
+        $locationProvider.html5Mode(true);
+
+        // Set routing.
+        $routeProvider.
+        when('/:slug', {
+            controller: 'PostController',
+            resolve: {}
+        }).otherwise({
+            redirectTo: '/'
+        });
+    };
 
 
-        ]).config(config);
-        // Route configuration below.
-        function config($routeProvider, $locationProvider) {
-            // HTML5 mode Required to avoid urls with # anchor tags.
-            $locationProvider.html5Mode(true);
-
-            // Set routing.
-            $routeProvider.
-            when('/:slug', {
-                controller: 'PostController',
-                resolve: {}
-            }).otherwise({
-                redirectTo: '/'
-            });
-        };
 
 
+/*IIFE Immediately Invoked Function Expression*/
 }());
