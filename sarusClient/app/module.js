cf976a43e909cccb55ea0d7f9d133816c62603db
Aashish <aashishmangal92@gmail.com>
2015-04-20 15:14:25 +0530

    initial commit

---
+++ sarusClient/app/module.js
@@ -0,0 +1,31 @@
+var addModule = {
+    "sarusAppadvertisement": true,
+    "logoImage": {
+        "scr": "http://1.bp.blogspot.com/-unf1wRW4SEs/VLTTbPZrwXI/AAAAAAAACrU/G0qJREyBQyE/s1600/ICC-Cricket-World-Cup-2015.gif",
+        "css": {
+            "height": "30px",
+            "width": "98px"
+        },
+    },
+    "backgroundColour": {
+        "Full": {
+            "background-color": "white",
+        },
+        "Header": {
+            "background-color": "rgb(236, 255, 178)",
+        },
+
+    },
+    "linkColor": {
+        "color": "green",
+    },
+    "linkColorActive": {
+        "color": "yellow",
+    },
+    "socialMedia": {
+        "facebook": true,
+        "twitter": true,
+        "linkedin": true,
+    },
+    "discussion": true //In progress 
+}
