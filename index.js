91bf9ccba66b0d6f34445a7abafb1ae679a69953
joseph dias <josephdias92@gmail.com>
2016-10-06 14:11:35 +0530

    build

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var gzippo = require('gzippo');
 var express = require('express');
-var app = express();
+var http = require('http');
+var gzippo = require('gzippo');
 
-app.use(express.logger('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+var app = express();
+app.use(gzippo.staticGzip('' + __dirname));
+app.use('/*', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+var server = http.createServer(app);
+server.listen(process.env.PORT || 5000);
