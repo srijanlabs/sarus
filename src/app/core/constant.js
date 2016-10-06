62cd42d6361784ade71375474720fd33147a40a9
joseph dias <josephdias92@gmail.com>
2016-10-06 11:19:23 +0530

    added google ads on infinitescroll

--- src/app/core/constant.js
+++ src/app/core/constant.js
@@ -2,7 +2,8 @@
   module.constant('COMMON', {
     numberOfRecordsPerPage: 1,
     ServerURL: 'http://sarus-openredesign.pantheonsite.io/api/',
-    FrontEndURLPattern: '/index/:q'
+    FrontEndURLPattern: '/:q',
+    query: 'q'
   });
   module.constant('adInfo', {
     clientId: 'ca-pub-7338620685919200'
