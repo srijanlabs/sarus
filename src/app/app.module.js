fe8d9d69786b0d063612ba36afe480a135a719d5
joseph dias <josephdias92@gmail.com>
2016-10-12 12:02:22 +0530

    Build file in gh-paages branch

--- src/app/app.module.js
+++
@@ -1,5 +0,0 @@
-(function(angular) {
-  angular.module('sarusApp', [
-    'ui.router'
-  ]);
-})(angular)
--- src/app/core/config.js
+++
@@ -1,6 +0,0 @@
-(function(module) {
-  module.config(function($locationProvider, $urlMatcherFactoryProvider) {
-    $locationProvider.html5Mode(true);
-    $urlMatcherFactoryProvider.strictMode(false)
-  });
-})(angular.module('sarusApp'));
